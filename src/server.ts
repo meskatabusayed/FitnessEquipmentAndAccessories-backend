9ca4416380498d6606237fb1dacfa6457e03748f
meskatabusayed <meskatabusayed@gmail.com>
2024-08-25 21:05:09 +0530

    server create done

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,17 @@
 import mongoose from "mongoose";
+import app from "./app";
+
 
 async function main() {
-    await mongoose.connect('mongodb://127.0.0.1:27017/test');
+    await mongoose.connect('mongodb+srv://fitnessEquipment:tsGRCFvQ4fRKF1vh@cluster0.fohhaen.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+
+    const port = 5000
+
+    app.listen(port, () => {
+        console.log(`Example app listening on port ${port}`)
+      })
   
-    // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+    
   }
+
+  main();
