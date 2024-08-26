61424644b6fd4af3d237d706ed3387d7ef357e92
meskatabusayed <meskatabusayed@gmail.com>
2024-08-26 15:27:24 +0530

    server setup and type assertion fixed

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,16 @@
 import mongoose from "mongoose";
 import app from "./app";
+import config from "./config";
+
 
 async function main() {
   try {
-    await mongoose.connect(
-      ""
-    );
+    await mongoose.connect(config.db_url as string);
 
-    const port = 5000;
+    
 
-    app.listen(port, () => {
-      console.log(`Example app listening on port ${port}`);
+    app.listen(config.port, () => {
+      console.log(`Example app listening on port ${config.port}`);
     });
   } catch (error) {
     console.log(error);
