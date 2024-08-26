f2a1c8f5ddb47a6cd6ddbcc556fbeec7f8b01e9a
meskatabusayed <meskatabusayed@gmail.com>
2024-08-26 15:13:54 +0530

    update server file

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,20 @@
 import mongoose from "mongoose";
 import app from "./app";
 
-
 async function main() {
-    await mongoose.connect('mongodb+srv://fitnessEquipment:tsGRCFvQ4fRKF1vh@cluster0.fohhaen.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+  try {
+    await mongoose.connect(
+      ""
+    );
 
-    const port = 5000
+    const port = 5000;
 
     app.listen(port, () => {
-        console.log(`Example app listening on port ${port}`)
-      })
-  
-    
+      console.log(`Example app listening on port ${port}`);
+    });
+  } catch (error) {
+    console.log(error);
   }
+}
 
-  main();
+main();
